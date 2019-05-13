11ec33172bd42b62c8d3e9aadebc2012e2ef2639
leo scott <leoscott@leo-scotts-MacBook-Pro.local>
2019-05-13 10:46:52 +0100

    modified epic to do json call

--- src/redux/epics/fetchMessage.js
+++ src/redux/epics/fetchMessage.js
@@ -1,17 +1,31 @@
-import {ofType} from 'redux-observable'
+import {ofType, combineEpics} from 'redux-observable'
 import {timer} from 'rxjs';
 import {mergeMap, map} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 const setLink = link => ({ type: 'setLink', link });
+const setImages = payload => ({ type: 'setImages', payload });
 
-export default action$ => action$.pipe(
-  ofType('fetch'),
-  mergeMap(() =>
-    timer(5000).pipe(
-      map(() => setLink({
-        message: 'Read more about us',
-        href: 'http://www.policyexpert.co.uk'
-      }))
+const epic1 = (action$, state$) =>
+  action$.pipe(
+    ofType('fetchInitImages'),
+    mergeMap(() =>
+      ajax.getJSON(`https://jsonplaceholder.typicode.com/photos`).pipe(
+        map(response => setImages(response.slice(0, 2)))
+      )
+    ),
+  );
+
+const epic2 = (action$, state$) =>
+  action$.pipe(
+    ofType('fetchImages'),
+    mergeMap(() =>
+      ajax.getJSON(`https://jsonplaceholder.typicode.com/photos`).pipe(
+        map(response => setImages(response.slice(0, state$.value.rendered + 1)))
+      )
     )
-  )
-);
+  );
+
+const rootEpic = combineEpics(epic1, epic2);
+
+export default rootEpic;
